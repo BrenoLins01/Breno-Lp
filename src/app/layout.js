b27de358ba94030be302e76e44f930895077817f
BrenoLins01 <142843483+BrenoLins01@users.noreply.github.com>
2024-01-23 22:58:57 -0300

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,33 +13,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <>
-      <Head>
-        {/* Adicione o script do pixel do Facebook aqui */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1504448120101551');
-              fbq('track', 'PageView');
-            `,
-          }}
-        />
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=1504448120101551&ev=PageView&noscript=1"
-          />
-        </noscript>
-      </Head>
       <html lang="en">
         <ScreenSizeProvider>
           <body className={inter.className}>{children}</body>
